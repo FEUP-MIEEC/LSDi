179b64b831ad9268ffac678dd22cb67adcf94f97
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2015-12-16 22:25:28 +0000

    Final Tweaks

--- Aula_Pratica_05/Ficheiros de Implementação/Parte 1/ROM_a.v
+++ Aula_Pratica_05/Ficheiros de Implementação/Parte 1/ROM_a.v
@@ -6,7 +6,10 @@ module ROM( endereco, dado );
 	
 	always @*
 		case (endereco)
-			//Ainda em falta
+			2’d0: dado = 8’b1101_0001;
+			2’d1: dado = 8’hAB;
+			2’d2: dado = 8’d29;
+			2’d3: dado = 8’b1010_0001;
 			
 			default: dado = 8'd0;
 		endcase
